49e700de353903840b4af2cc3838920196d62ecf
Abhilekh Adhikari <abhilekhadhikari@MacBook-Pro.local>
2021-08-30 13:46:07 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function removeAlert(element) {
+  element.parentNode.remove();
+}
+
+function alertCart() {
+  alert("Your cart is empty");
+}
+
+function hoverIn(element) {
+  element.src = "images/assets/succulents-2.jpg";
+}
+
+function hoverOut(element) {
+  element.src = "images/assets/succulents-1.jpg";
+}
